dbcf460b48ff315c9d444f44f9b7162e745a25cc
Brian Muugweru <brianmugweru@gmail.com>
2017-01-03 14:23:22 +0300

    configure checkbody properly and add user to database

--- routes/user.js
+++ routes/user.js
@@ -1,44 +1,55 @@
 var User          = require('../models/user');
 var Role          = require('../models/role');
+
 module.exports    = {
   new : function(req, res){
     var message='';
+    var errors='';
     res.render('pages/register', {
-        message : req.flash('signupMessage'),
-        title   : "Sign Up"
+        errors:errors,
+        message : req.flash('signupMess'),
+        title   : "Sign Up",
     });
   },
 
   create : function(req, res){
    Role.findOne({ name:req.body.role}, function(err, role){
-      if(err) return(err);
+     if(err) return(err);
 
       req.checkBody('fname','First name is required').notEmpty();
       req.checkBody('lname','Last name is required').notEmpty();
       req.checkBody('email','Email is required').notEmpty();
       req.checkBody('password','Password is required').notEmpty();
       req.checkBody('password2','Please you need to confirm your password').notEmpty();
-      req.checkBody('password', 'The passwords do not match').isEqual('password2');
+      req.checkBody('password2', 'The passwords do not match').equals(req.body.password);
       req.checkBody('email','Email is invalid').isEmail();
 
       var errors = req.validationErrors();
       if(errors){
-        res.redirect('/register');
+       res.render('pages/register',{
+          errors:errors,
+          title:'Sign up',
+          message: req.flash('signupMess')
+        });
       }
 
-      var fname=req.body.fname;
-      var lname=req.body.lname;
-      var email=req.body.email;
-      var username=req.body.username;
-      var password=req.body.password;
-      var password2=req.body.password2;
+      var user = new User();
+      user.fname=req.body.fname;
+      user.lname=req.body.lname;
+      user.email=req.body.email;
+      user.username=req.body.username;
+      user.password=req.body.password;
+
       User.getUserByUsername({username: req.body.username}, function(err, foundUser, done){
         var message = 'That username is already taken';
+        var errors = '';
         if(err) throw err;
 
         if(foundUser){
-          res.render('users/new', {
-            message: message
+          res.render('pages/register', {
+            errors:errors,
+            message: req.flash('signupMess'),
+            title:"sign Up"
           });
         } else {
           console.log('You have no register errors');
