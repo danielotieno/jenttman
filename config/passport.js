77fe6121647fb004ba7e9a01d0ce17f3912f78cf
Brian Muugweru <brianmugweru@gmail.com>
2017-01-03 14:58:27 +0300

    change User directory to correct one

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 var passport         = require('passport');
 var LocalStrategy    = require('passport-local').Strategy;
-var User             = require('../app/models/user');
+var User             = require('../models/user');
 
 module.exports       = function(passport) {
   passport.serializeUser(function(user, done) {
