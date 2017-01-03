9a014d0ba200a8e9558f4705232d8ffe0d929425
Brian Muugweru <brianmugweru@gmail.com>
2017-01-03 14:21:44 +0300

    add methods getUserByUsername and getUserById to model to find users by username and by id from the database

--- models/user.js
+++ models/user.js
@@ -13,18 +13,27 @@ var UserSchema = Schema({
     role:     [{type: Schema.Types.ObjectId, ref:Role }]
 });
 
+var User = module.exports = mongoose.model('User', UserSchema);
 
 module.exports.createUser = function(newUser,callback){
   bcrypt.genSalt(10,function(err,salt){
     if(err) throw err;
-    bcrypt.hash(newUser.local.password,salt,function(err,hash){
-      newUser.local.password = hash;
-      newUser.local.save(callback);
+    bcrypt.hash(newUser.password,salt,function(err,hash){
+      newUser.password = hash;
+      newUser.save(callback);
     });
   });
 };
 
 module.exports.comparePassword = function(candidatePassword, hash, callback){
   bcrypt.compare(candidatePassword, hash, callback);
+};
+
+module.exports.getUserByUsername = function(username, callback){
+  User.findOne(username,callback);
+};
+
+module.exports.getUserById = function(id,callback){
+  User.findById(id,callback);
 };
 
