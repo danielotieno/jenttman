cd782b7c421d81ea1dc16561ec2665b3b06ed35c
Brian Muugweru <brianmugweru@gmail.com>
2017-01-09 01:33:54 +0300

    remove size and stock fields and place them in a different schema to enforce database relationships

--- models/fashion.js
+++ models/fashion.js
@@ -1,16 +1,18 @@
-var mongoose   = require('mongoose');
-var Schema     = mongoose.Schema;
-var User       = require('./user');
-var Category   = require('./category');
+/*
+ * Fill in fashions that the website has in stock
+ */
+var mongoose = require('mongoose');
+var Schema   = mongoose.Schema;
+var User     = require('./user');
+var Category = require('./category');
+var Size     = require('./size');
 
 var FashionSchema = Schema({
-  name : {type:String, index:true},
-  stock: {type:Number},
+  name:     {type:String, index:true},
   category: [{type: Schema.Types.ObjectId, ref:Category}],
-  brands: {type: String},
-  description:{ type:String},
-  sizes:{type:String},
-  user: [{type:Schema.Types.ObjectId, ref:User}]
+  brands:   {type: String},
+  desc:     { type:String},
+  user:     [{type:Schema.Types.ObjectId, ref:User}]
 });
 
 module.exports = mongoose.model('Fashion', FashionSchema);
