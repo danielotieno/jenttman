d1211ee137e8e72759477ab688f1388447fe3e81
CherutoMercy <m.thirty100@gmail.com>
2017-01-10 09:57:19 +0300

    redirect add category to views

--- routes/category.js
+++ routes/category.js
@@ -2,6 +2,14 @@ var User = require('../models/user');
 var Category = require('../models/category');
 
 module.exports = {
+  index : function(req, res){
+    Category.find({}, function(err, category){
+      if(err) res.send(err);
+      res.render('admin/category',{
+          category:category
+      });
+    });
+  },
   new : function(req, res){
     res.render('admin/category');
   },
@@ -19,13 +27,13 @@ module.exports = {
       if(foundCategory) res.send("category already exists idiot");
       else{
         var category = new Category();
-        category.name = req.body.category;
+        category.name = req.body.name;
 
         category.save(function(err, category){
           if(err) res.send(err);
           
-          res.flash('message', 'Category saved successfully');
-          res.redirect('/admin/index/add');
+          //res.flash('message', 'Category saved successfully');
+          res.redirect('/admin/add');
         });
       }
     });
