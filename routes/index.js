0d19de3180bed5b5732343a298adafd352dc5c53
CherutoMercy <m.thirty100@gmail.com>
2017-01-10 10:11:07 +0300

    add fashion category and size routes

--- routes/index.js
+++ routes/index.js
@@ -42,21 +42,33 @@ router.post('/login',          sessionRoutes.create);
 router.get('/logout',          sessionRoutes.delete);
 
 /*
- * @session routes 
+ * @fashionroutes 
 */
-router.get('/admin/index',        fashionRoutes.index);
+router.get('/admin/fashions',        fashionRoutes.index);
 router.get('/user/item',          fashionRoutes.single);
-router.post('/admin/create',      fashionRoutes.add);
-router.get('/admin/create',       fashionRoutes.new);
+router.get('/admin/create',      fashionRoutes.new);
+router.post('/admin/fashions',       fashionRoutes.add);
 router.post('/admin/update',      fashionRoutes.update);
-router.get('/admin/index/delete', fashionRoutes.delete);
+router.get('/admin/index/edit',   fashionRoutes.edit);
+router.get('/admin/delete', fashionRoutes.delete);
+router.post('/admin/another',      fashionRoutes.addsize);
 
 /*
  * @category Routes
  */
-router.get('/admin/categoty/new', categoryRoutes.new);
-router.get('/admin/categoty',     categoryRoutes.get);
-router.post('admin/category/add', categoryRoutes.add);
+router.get('/admin/add', categoryRoutes.index);
+router.get('/admin/new',     categoryRoutes.new);
+router.post('admin/add', categoryRoutes.add);
+
+/*
+ * @size Routes
+ */
+router.get('/admin/another', sizeRoutes.index);
+router.get('/admin/newer', sizeRoutes.new);
+//router.get('/admin/categoty',     categoryRoutes.get);
+router.post('/admin/another', sizeRoutes.add);
+
+
 
 /*
  * @Role routes
