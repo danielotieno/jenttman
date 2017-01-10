3530fd54ea656bba8b81023f627a2846360dff64
CherutoMercy <m.thirty100@gmail.com>
2017-01-10 09:54:34 +0300

    change brands to brand in schema and add slug

--- models/fashion.js
+++ models/fashion.js
@@ -10,7 +10,8 @@ var Size     = require('./size');
 var FashionSchema = Schema({
   name:     {type:String, index:true},
   category: [{type: Schema.Types.ObjectId, ref:Category}],
-  brands:   {type: String},
+  brand:   {type: String},
+  slug  : {type : String, default : ""},
   desc:     { type:String},
   user:     [{type:Schema.Types.ObjectId, ref:User}]
 });
