9e55a63a351dbb7d501d6501128cc5c5da206bc1
Brian Muugweru <brianmugweru@gmail.com>
2017-02-01 19:10:00 +0300

    add routes to view a single fashion item for normal users

--- routes/index.js
+++ routes/index.js
@@ -51,6 +51,7 @@ router.get('/logout',          sessionRoutes.delete);
 */
 router.get('/admin/fashions',           fashionRoutes.index);
 router.get('/admin/fashion/item/:id',   fashionRoutes.single);
+router.get('/fashion/item/:id',   fashionRoutes.single);
 router.get('/admin/fashion/create',     fashionRoutes.new);
 router.post('/admin/fashion/add',       upload.single('upload'),     fashionRoutes.add);
 router.get('/admin/fashion/edit/:id',   fashionRoutes.edit);
