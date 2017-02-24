39f37b24df3f55009b9b8478006e2fc09b15174d
CherutoMercy <m.thirty100@gmail.com>
2017-02-24 13:53:56 +0300

    add billing details routes

--- routes/orders.js
+++ routes/orders.js
@@ -6,9 +6,51 @@ var Fashion  = require('../models/fashion');
 var User     = require('../models/user');
 var Category = require('../models/category');
 var Size     = require('../models/size');
+var Cart= require('../models/cart')
+var Sesscart = require('../models/sesscart');
+var Orders     = require('../models/orders');
 
 module.exports = {
   //route to view template with fashions already in the system
+
+order : function(req, res){
+    Fashion.find({}, function(err, Fashion){
+      if(err) res.send(err);
+      var cart = new Sesscart(req.session.cart);
+      res.render('pages/pay',{
+          products:cart.generateArray(),
+          totalPrice:cart.totalPrice
+      });
+    });
+  },
+
+  billing : function(req, res,next){
+        User.findOne({username:req.body.username}, function(err, foundUser){
+          if(err) res.send(err);
+
+          var orders      = new Orders();
+          orders.fname    = req.body.fname;
+          orders.lname    = req.body.lname;
+          orders.companyname    = req.body.companyname;
+          orders.email   = req.body.email;
+          orders.country    = req.body.country;
+          orders.address= req.body.address;
+          orders.town= req.body.town;
+          orders.county= req.body.county;
+          orders.postalcode  = req.body.postalcode;
+
+          console.log("orders at this stage", orders);
+         
+          orders.save(function(err, orders){
+            if(err) res.send(err);
+
+            res.redirect('/mpesa/:id');
+          });
+        });
+    
+  },
+
+
   index : function(req, res){
     Fashion.find({}, function(err, fashion){
       if(err) res.send(err);
