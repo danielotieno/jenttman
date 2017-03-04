4e5f0c04608fe4f875bace7804423b1c69909375
CherutoMercy <m.thirty100@gmail.com>
2017-03-04 18:31:26 +0300

    add blog database model

---
+++ models/blog.js
@@ -0,0 +1,15 @@
+/*
+ * Category schema to fill the categories that the website is selling
+ */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var User = require('./user');
+
+var BlogSchema = Schema({
+  title  :{type:String},
+  photo  :{type:String},
+  content:{type:String},
+  User   :[{type : Schema.Types.ObjectId, ref:User }]
+});
+
+module.exports = mongoose.model('Blog', BlogSchema);
