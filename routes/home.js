61822a89d4ac8ffb53b9d990fd2fe781485b3586
Brian Muugweru <brianmugweru@gmail.com>
2017-03-17 14:53:00 +0300

    configure route not yet working for sending mails from google

--- routes/home.js
+++ routes/home.js
@@ -3,8 +3,25 @@
  * Routes for rendering home index page
  */
 
-var Fashion = require('../models/fashion.js');
-var Category = require('../models/category.js');
+var Fashion    = require('../models/fashion.js');
+var Category   = require('../models/category.js');
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+var secrets = require('../config/secret.js');
+var config = require('../config/client-secret.js');
+var xouath2 = require('xoauth2');
+
+/*var transporter = nodemailer.createTransport(smtpTransport({
+  service : "Gmail",
+  xouath2 : {
+    user : secrets.username,
+    pass : secrets.password,
+    clientId : config.client_id,
+    clientSecret : config.client_secret,
+    refreshToken : '1/7SIJEjPYGlqA71nw9_THVhdDENYFNIk9WTnERvoXpANthlqnwVnm0xD0hHJa_yle'
+  }
+}));
+*/
 
 module.exports = {
   index : function(req, res){
@@ -19,6 +36,21 @@ module.exports = {
 
   contact : function(req, res){
       res.render('pages/contact')
+  },
+
+  mailer : function(req, res, next){
+   /* var mailOptions = {
+      from : 'yourdaddy@gmail.com ',
+      to : 'bmirauri@gmail.com',
+      subject : 'testing out nodemailer',
+      text : 'just hoping it works out properly'
+    };
+    transporter.sendMail(mailOptions, function(err,info){
+      if(err) return next(err);
+      return res.send("mail sent successfully");
+    });
+    */
+    res.send("Node mail sender under configuration");
   },
 
   categories : function(req, res){
