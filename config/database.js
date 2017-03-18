5af7f104917ff00b76b94900c7cbd336ee146c20
Brian Muugweru <brianmugweru@gmail.com>
2017-03-18 17:42:59 +0300

    add details for accessing nodemailer and sending mails from application

--- config/database.js
+++ config/database.js
@@ -1,15 +1,24 @@
-
+/*
+ * FOR THIS TO WORK YOU MUST ALLOW LESS SECURE APPS IN YOUR GMAIL SETTINGS TO ACCESS
+ * YOUR EMAIL ACCOUNT. JUST DONT PUSH IT TO GITHUB SO MAKE SURE YOU GITIGNORE
+ */
 module.exports = [
   {
     'environment': 'development',
     'url'       : 'mongodb://localhost/jenttman',
-    'secret'    : 'anystringoftext'
+    'secret'    : 'anystringoftext',
+    'email'       : 'YOUR EMAIL ADDRESS',
+    'password'    : 'PASSWORD FOR YOUR EMAIL ADDRESS'
+
   },
 
   {
     'environment': 'production',
     'url'       : process.env.MONGOLAB_URI,
-    'secret'    : 'anystringoftext'
+    'secret'    : 'anystringoftext',
+    'email'       : process.env.USEREMAIL,
+    'password'    : process.env.USERPASSWORD
+
   }
 ];
 
